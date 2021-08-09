e8e599cd9c0fc01029f7c44b8fbba1eee245d3ab
SNOWIN J <20103104@hit.edu.in>
2021-08-09 23:34:12 +0530

    adding files for githun pages

---
+++ src/components/search-box/search.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './search.styles.css';
+
+export const Search = ({ placeholder, handleChange }) => {
+
+    return (
+
+        <input
+            className="search"
+            placeholder={placeholder}
+            onChange={handleChange}
+        />
+
+    );
+
+}
